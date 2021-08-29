6c7b15a1cd7a433c4ea53e4830c0d0ca18aa0bbe
gigiftttgi <natcha_si@cmu.ac.th>
2021-08-29 13:11:39 +0700

    1

--- src/components/historyTem.tsx
+++ src/components/historyTem.tsx
@@ -8,43 +8,27 @@ import { type } from 'node:os';
 
 
 const HistoryTem = () => {
-  
-  function useQuery() {
-    return new URLSearchParams(useLocation().search);
-  }
-let query = useQuery();
-    
-
-interface hisList {
-  key : string;
-  value : string;
-}
-
-const allhis : Array<hisList> = []
 
       const {alldata,selected, setSelected} = HistoryPrice()
-      if(alldata == "error")
-      {
-        console.log("error")
-      }
-     const a = alldata.split("{").join("").split("}").join("").split("\"").join("")
-     const all = a.split(":").join(" - ").split(",")
+      const a = alldata.split("{").join("").split("}").join("").split("\"").join("")
+      const all = a.split(":").join(" - ").split(",")
    
-
+      const checkerror = () =>
+      {
+        if(alldata!="error")
+          return <Showhistory list = {all}/>;
+        else
+          return <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>
+      }
     
     return(
       <div className='text-center space-y-3'>
      <p className='text-2xl font-semibold'>Historical price</p>
      
-     {alldata === ""? <p className='text-2xl'>Loading ...</p> : 
+     {alldata === "" ? <p className='text-2xl'>Loading ...</p> : 
      <div>
-     <p className='text-xl font-semibold'> ( From {query.get("start")} To {query.get("end")})</p>
-      { <Showhistory list = {all}/>}
+      { checkerror() }
  </div> }
-     
-    
-     {/* {alldata === "error"? <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>: 
-     <p>There was an error. Please try again later.</p>} */}
    </div>
 
     )
--- src/components/showhistory.tsx
+++ src/components/showhistory.tsx
@@ -3,11 +3,20 @@ import { useLocation } from "react-router-dom";
   interface prop {
     list : string[]
 }
+  
+   
+      
 
 const Showhistory : React.FC<prop> = ({list}) => {
-
-    return(
+ 
+    function useQuery() {
+      return new URLSearchParams(useLocation().search);
+    }
+  let query = useQuery();
+    
+  return(
         <div>
+            <p className='text-xl font-semibold'> ( From {query.get("start")} To {query.get("end")})</p>
             <ul>
                 { list.map(a => 
                     <li className='text-xl'>{a} THB</li> )} 
